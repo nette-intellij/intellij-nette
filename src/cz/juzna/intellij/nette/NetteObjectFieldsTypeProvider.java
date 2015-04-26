0e0a3bfa36ed002ff214af746301e595ccb18618
David Matejka <matej21@matej21.cz>
2015-04-26 19:11:37 +0200

    NetteObjectFieldsTypeProvider: refactored; deprecated PhpTypeProvider replaced by PhpTypeProvider2

--- src/cz/juzna/intellij/nette/NetteObjectFieldsTypeProvider.java
+++ src/cz/juzna/intellij/nette/NetteObjectFieldsTypeProvider.java
@@ -1,33 +1,66 @@
 package cz.juzna.intellij.nette;
 
 import com.intellij.openapi.project.DumbService;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.util.containers.HashMap;
 import com.jetbrains.php.PhpIndex;
 import com.jetbrains.php.lang.psi.elements.FieldReference;
 import com.jetbrains.php.lang.psi.elements.Method;
+import com.jetbrains.php.lang.psi.elements.PhpNamedElement;
 import com.jetbrains.php.lang.psi.resolve.types.PhpType;
-import com.jetbrains.php.lang.psi.resolve.types.PhpTypeProvider;
+import com.jetbrains.php.lang.psi.resolve.types.PhpTypeProvider2;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
-/**
- *
- */
-public class NetteObjectFieldsTypeProvider implements PhpTypeProvider {
+
+public class NetteObjectFieldsTypeProvider implements PhpTypeProvider2 {
+
+	static char key = '\u0223';
+
 	@Override
-	public PhpType getType(PsiElement e) {
-		if (DumbService.getInstance(e.getProject()).isDumb()) return null;
+	public char getKey() {
+		return key;
+	}
 
-		if (e instanceof FieldReference) {
-			FieldReference field = (FieldReference) e;
-			HashMap<String, Collection<Method>> fields = FieldFinder.findMagicFields(field.getClassReference().getType(), PhpIndex.getInstance(e.getProject()));
+	@Nullable
+	@Override
+	public String getType(PsiElement e) {
+		if (DumbService.getInstance(e.getProject()).isDumb()) {
+			return null;
+		}
+		if (!(e instanceof FieldReference)) {
+			return null;
+		}
+		FieldReference field = (FieldReference) e;
+		if (field.getClassReference() == null) {
+			return null;
+		}
+		PhpIndex phpIndex = PhpIndex.getInstance(e.getProject());
+		HashMap<String, Collection<Method>> fields = FieldFinder.findMagicFields(field.getClassReference().getType(), phpIndex);
+		if (!fields.containsKey(field.getName())) {
+			return null;
+		}
+		PhpType phpType = MagicFieldsTypesHelper.extractTypeFromMethodTypes(fields.get(field.getName()));
+		if (phpType == null) {
+			return null;
+		}
+		return phpType.toStringResolved();
+	}
 
-			if (fields.containsKey(field.getName())) {
-				return MagicFieldsTypesHelper.extractTypeFromMethodTypes(fields.get(field.getName()));
-			}
+	@Override
+	public Collection<? extends PhpNamedElement> getBySignature(String s, Project project) {
+		PhpIndex index = PhpIndex.getInstance(project);
+		Collection<PhpNamedElement> result = new ArrayList<PhpNamedElement>();
+		for (String type : s.split("\\|")) {
+			result.addAll(index.getAnyByFQN(type));
 		}
 
-		return null;
+
+		return result;
 	}
+
+
 }
