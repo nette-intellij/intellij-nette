4c5f25867b91e7c5fe23203f28c5b8bf72489da0
David Matejka <matej21@matej21.cz>
2015-05-08 13:38:10 +0200

    Added event listener generator

---
+++ src/cz/juzna/intellij/nette/actions/GenerateInlineListenerAction.java
@@ -0,0 +1,125 @@
+package cz.juzna.intellij.nette.actions;
+
+import com.intellij.codeInsight.CodeInsightActionHandler;
+import com.intellij.codeInsight.actions.CodeInsightAction;
+import com.intellij.lang.LanguageCodeInsightActionHandler;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.DumbService;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiErrorElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiWhiteSpace;
+import com.intellij.psi.codeStyle.CodeStyleManager;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
+import com.jetbrains.php.PhpIndex;
+import com.jetbrains.php.lang.PhpLanguage;
+import com.jetbrains.php.lang.psi.PhpPsiElementFactory;
+import com.jetbrains.php.lang.psi.elements.*;
+import cz.juzna.intellij.nette.utils.ListenerGeneratorUtil;
+import cz.juzna.intellij.nette.utils.PhpIndexUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+
+
+public class GenerateInlineListenerAction extends CodeInsightAction {
+
+	private static LanguageCodeInsightActionHandler HANDLER = new GenerateInlineListenerHandler();
+
+	@Override
+	protected boolean isValidForFile(@NotNull Project project, @NotNull Editor editor, @NotNull PsiFile file) {
+		return !DumbService.getInstance(file.getProject()).isDumb() &&
+				file.getLanguage().is(PhpLanguage.INSTANCE) && HANDLER.isValidFor(editor, file);
+	}
+
+	@NotNull
+	@Override
+	protected CodeInsightActionHandler getHandler() {
+		return HANDLER;
+	}
+
+	private static class GenerateInlineListenerHandler implements LanguageCodeInsightActionHandler {
+		@Override
+		public boolean isValidFor(Editor editor, PsiFile psiFile) {
+			PsiElement el = psiFile.findElementAt(editor.getCaretModel().getOffset());
+
+			return getField(el) != null;
+		}
+
+		@Override
+		public void invoke(@NotNull final Project project, @NotNull Editor editor, @NotNull final PsiFile psiFile) {
+			PsiElement el = psiFile.findElementAt(editor.getCaretModel().getOffset());
+
+			Field field = getField(el);
+			if (field == null) {
+				return;
+			}
+			String method = ListenerGeneratorUtil.createListenerTemplate(field, el, null);
+			final PhpPsiElement fc = PhpPsiElementFactory.createFromText(project, Function.class, method);
+
+			final AssignmentExpression expr = getAssignment(el);
+			if (expr == null) {
+				return;
+			}
+			ApplicationManager.getApplication().runWriteAction(new Runnable() {
+				@Override
+				public void run() {
+					expr.getLastChild().replace(fc);
+					PsiElement el = expr.getLastChild();
+					CodeStyleManager.getInstance(project)
+							.reformatText(psiFile, el.getTextOffset(), el.getTextOffset() + el.getTextLength());
+				}
+			});
+		}
+
+		@Override
+		public boolean startInWriteAction() {
+			return false;
+		}
+
+		private static Field getField(PsiElement el) {
+			FieldReference ref = getFieldReference(el);
+			if (ref == null) {
+				return null;
+			}
+			if (ref.getName() == null || !ref.getName().startsWith("on") || ref.getClassReference() == null) {
+				return null;
+			}
+			PhpIndex phpIndex = PhpIndex.getInstance(el.getProject());
+			Collection<PhpClass> classes = PhpIndexUtil.getClasses(ref.getClassReference().getType(), phpIndex);
+			if (classes.size() > 0) {
+				return classes.iterator().next().findFieldByName(ref.getName(), false);
+			}
+			return null;
+		}
+
+		private static FieldReference getFieldReference(PsiElement el) {
+			AssignmentExpression expr = getAssignment(el);
+
+			if (expr == null ||
+					!(expr.getFirstChild() instanceof ArrayAccessExpression)
+					|| !(expr.getFirstChild().getFirstChild() instanceof FieldReference)
+					) {
+				return null;
+			}
+			return (FieldReference) expr.getFirstChild().getFirstChild();
+		}
+
+		private static AssignmentExpression getAssignment(PsiElement el) {
+			if (el instanceof LeafPsiElement && el.getText().equals(";")
+					&& el.getPrevSibling() instanceof PsiWhiteSpace
+					&& el.getPrevSibling().getPrevSibling() instanceof AssignmentExpression) {
+				return (AssignmentExpression) el.getPrevSibling().getPrevSibling();
+			}
+			if (el instanceof PsiWhiteSpace
+					&& el.getPrevSibling() instanceof Statement
+					&& el.getPrevSibling().getFirstChild() instanceof AssignmentExpression
+					&& el.getPrevSibling().getFirstChild().getLastChild() instanceof PsiErrorElement) {
+				return (AssignmentExpression) el.getPrevSibling().getFirstChild();
+			}
+			return null;
+		}
+	}
+}
---
+++ src/cz/juzna/intellij/nette/utils/ListenerGeneratorUtil.java
@@ -0,0 +1,174 @@
+package cz.juzna.intellij.nette.utils;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.jetbrains.php.codeInsight.PhpCodeInsightUtil;
+import com.jetbrains.php.lang.PhpLangUtil;
+import com.jetbrains.php.lang.documentation.phpdoc.psi.PhpDocMethod;
+import com.jetbrains.php.lang.psi.elements.*;
+import com.jetbrains.php.lang.psi.resolve.types.PhpType;
+import com.yourkit.util.Strings;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+public class ListenerGeneratorUtil {
+
+	private static ParametersResolver[] resolvers = {new PhpDocMethodParametersResolver(), new PhpDocFieldParametersResolver()};
+
+	public static String createListenerTemplate(Field field, PsiElement scope, @Nullable String name) {
+		List<ParameterInfo> parameters = null;
+		for (ParametersResolver resolver : resolvers) {
+			parameters = resolver.getParameters(field);
+			if (parameters != null) {
+				break;
+			}
+		}
+
+		StringBuilder tpl = new StringBuilder();
+		if (name == null) {
+			tpl.append("function (");
+
+		} else {
+			tpl.append("public function ").append(name).append("(");
+		}
+		if (parameters != null) {
+			int i = 0;
+			for (ParameterInfo parameter : parameters) {
+				if (i > 0) {
+					tpl.append(", ");
+				}
+				PhpType declaredType = parameter.getType();
+				if (declaredType != null) {
+					String qName = PhpCodeInsightUtil.createQualifiedName(getScopeElement(scope), declaredType.toString());
+					tpl.append(qName).append(" ");
+				}
+				tpl.append("$").append(parameter.getName());
+				i++;
+			}
+		}
+		tpl.append("){\n\n}\n");
+
+		return tpl.toString();
+	}
+
+
+	private static class ParameterInfo {
+		private String name;
+		private PhpType type;
+
+		public ParameterInfo(String name, @Nullable PhpType type) {
+			this.name = name;
+			this.type = type;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		@Nullable
+		public PhpType getType() {
+			return type;
+		}
+	}
+
+	private interface ParametersResolver {
+
+		@Nullable
+		public List<ParameterInfo> getParameters(@NotNull Field field);
+
+	}
+
+	private static class PhpDocMethodParametersResolver implements ParametersResolver {
+
+		@Nullable
+		@Override
+		public List<ParameterInfo> getParameters(@NotNull Field field) {
+			if (field.getContainingClass() == null) {
+				return null;
+			}
+			Method docMethod = field.getContainingClass().findMethodByName(field.getName());
+			if (docMethod == null || !(docMethod instanceof PhpDocMethod)) {
+				return null;
+			}
+			ArrayList<ParameterInfo> parameters = new ArrayList<ParameterInfo>();
+			int i = 0;
+			for (Parameter parameter : docMethod.getParameters()) {
+				String name = parameter.getName();
+				if (name.length() == 0) {
+					name = "param" + i;
+				}
+				PhpType declaredType = parameter.getDeclaredType();
+				if (declaredType.getTypes().size() != 1 || !declaredType.toString().startsWith("\\")) {
+					declaredType = null;
+				}
+				parameters.add(new ParameterInfo(name, declaredType));
+				i++;
+			}
+
+			return parameters;
+		}
+	}
+
+	private static class PhpDocFieldParametersResolver implements ParametersResolver {
+
+		@Nullable
+		@Override
+		public List<ParameterInfo> getParameters(@NotNull Field field) {
+			if (field.getDocComment() == null || field.getDocComment().getVarTag() == null) {
+				return null;
+			}
+			String description = field.getDocComment().getVarTag().getTagValue();
+			int start = description.indexOf("(");
+			int end = description.indexOf(")");
+			if (start == -1 || end == -1 || start > end) {
+				return null;
+			}
+			List<ParameterInfo> parameters = new ArrayList<ParameterInfo>();
+			String parametersStr = description.substring(start + 1, end);
+			int i = 0;
+			for (String param : parametersStr.split(",")) {
+				param = param.trim();
+				String type = param.contains(" ") ? param.substring(0, param.indexOf(" ")) : null;
+				type = toFqn(type, field);
+				String varName = param.contains("$") ? param.substring(param.indexOf("$") + 1) : "param" + i;
+				parameters.add(new ParameterInfo(varName, type != null ? (new PhpType()).add(type) : null));
+				i++;
+			}
+
+
+			return parameters;
+		}
+
+		private static String toFqn(String name, Field field) {
+			if (name == null || PhpLangUtil.isFqn(name)) {
+				return name;
+			}
+			PhpPsiElement parent = getScopeElement(field);
+
+			Map<String, String> aliases = PhpCodeInsightUtil.getAliasesInScope(parent);
+			String[] parts = name.split("\\\\", 2);
+			if (!aliases.containsKey(parts[0])) {
+				return null;
+			}
+			parts[0] = aliases.get(parts[0]);
+
+			return Strings.join("", parts, "\\", false);
+		}
+	}
+
+
+	@NotNull
+	private static PhpPsiElement getScopeElement(PsiElement el) {
+		PhpPsiElement parent = PsiTreeUtil.getParentOfType(el, PhpNamespace.class);
+		if (parent == null) {
+			parent = (PhpPsiElement) el.getContainingFile();
+		}
+
+		return parent;
+	}
+}
