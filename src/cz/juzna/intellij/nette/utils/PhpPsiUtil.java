9d29a15400c2c0dfa5cf6c7415adb7d59426705e
David Matejka <matej21@matej21.cz>
2015-07-12 15:41:50 +0200

    PhpPsiUtils - getClassesInFile: ignore PhpExpressionCodeFragment

--- src/cz/juzna/intellij/nette/utils/PhpPsiUtil.java
+++ src/cz/juzna/intellij/nette/utils/PhpPsiUtil.java
@@ -1,6 +1,7 @@
 package cz.juzna.intellij.nette.utils;
 
 import com.intellij.psi.PsiElement;
+import com.jetbrains.php.lang.psi.PhpExpressionCodeFragment;
 import com.jetbrains.php.lang.psi.PhpFile;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
 
@@ -11,7 +12,7 @@ public class PhpPsiUtil {
 
 	public static Map<String, PhpClass> getClassesInFile(PsiElement el) {
 		Map<String, PhpClass> classesInFile = new HashMap<String, PhpClass>();
-		if (el.getContainingFile() instanceof PhpFile) {
+		if (el.getContainingFile() instanceof PhpFile && !(el.getContainingFile() instanceof PhpExpressionCodeFragment)) {
 			for (PhpClass cls : com.jetbrains.php.lang.psi.PhpPsiUtil.findAllClasses((PhpFile) el.getContainingFile())) {
 				classesInFile.put(cls.getFQN(), cls);
 			}
