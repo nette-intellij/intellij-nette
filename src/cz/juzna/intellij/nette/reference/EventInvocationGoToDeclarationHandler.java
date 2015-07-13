7376dfa9fb6b135dbe027ed7fe1947406e602e29
David Matejka <matej21@matej21.cz>
2015-07-13 23:43:47 +0200

    added EventInvoicationGoToDeclarationHandler [closes #15]

---
+++ src/cz/juzna/intellij/nette/reference/EventInvocationGoToDeclarationHandler.java
@@ -0,0 +1,29 @@
+package cz.juzna.intellij.nette.reference;
+
+import com.intellij.codeInsight.navigation.actions.GotoDeclarationHandlerBase;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
+import com.jetbrains.php.PhpIndex;
+import com.jetbrains.php.lang.documentation.phpdoc.psi.PhpDocMethod;
+import com.jetbrains.php.lang.psi.elements.Field;
+import com.jetbrains.php.lang.psi.elements.Method;
+import com.jetbrains.php.lang.psi.elements.MethodReference;
+import com.jetbrains.php.lang.psi.elements.PhpClass;
+import cz.juzna.intellij.nette.utils.EventUtil;
+import org.jetbrains.annotations.Nullable;
+
+
+public class EventInvocationGoToDeclarationHandler extends GotoDeclarationHandlerBase {
+	@Nullable
+	@Override
+	public PsiElement getGotoDeclarationTarget(PsiElement psiElement, Editor editor) {
+		if (!(psiElement instanceof LeafPsiElement) || !(psiElement.getParent() instanceof MethodReference)) {
+			return null;
+		}
+		for (Field field : EventUtil.getEventDeclarations((MethodReference) psiElement.getParent())) {
+			return field;
+		}
+		return null;
+	}
+}
---
+++ src/cz/juzna/intellij/nette/utils/EventUtil.java
@@ -0,0 +1,33 @@
+package cz.juzna.intellij.nette.utils;
+
+import com.jetbrains.php.lang.documentation.phpdoc.psi.PhpDocMethod;
+import com.jetbrains.php.lang.psi.elements.Field;
+import com.jetbrains.php.lang.psi.elements.Method;
+import com.jetbrains.php.lang.psi.elements.MethodReference;
+import com.jetbrains.php.lang.psi.elements.PhpClass;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class EventUtil {
+
+	@NotNull
+	public static Collection<Field> getEventDeclarations(MethodReference methodReference) {
+		String eventName = methodReference.getName();
+		if (eventName == null || !eventName.startsWith("on")) {
+			return Collections.emptyList();
+		}
+		Collection<Field> fields = new ArrayList<Field>();
+		for (PhpClass cls : ClassFinder.getFromMemberReference(methodReference)) {
+			Method method = cls.findMethodByName(eventName);
+			if (method == null || method instanceof PhpDocMethod) {
+				fields.add(cls.findFieldByName(eventName, false));
+			}
+		}
+
+		return fields;
+	}
+
+}
