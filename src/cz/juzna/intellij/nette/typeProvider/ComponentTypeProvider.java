6e862b331df36364e9695421f67ed81289bed342
David Matejka <matej21@matej21.cz>
2017-08-26 11:54:50 +0200

    upgrade ComponentTypeProvider to PhpTypeProvider3

--- src/cz/juzna/intellij/nette/typeProvider/ComponentTypeProvider.java
+++ src/cz/juzna/intellij/nette/typeProvider/ComponentTypeProvider.java
@@ -11,21 +11,21 @@ import com.jetbrains.php.lang.psi.elements.PhpClass;
 import com.jetbrains.php.lang.psi.elements.PhpNamedElement;
 import com.jetbrains.php.lang.psi.elements.PhpTypedElement;
 import com.jetbrains.php.lang.psi.resolve.types.PhpType;
-import com.jetbrains.php.lang.psi.resolve.types.PhpTypeProvider2;
+import com.jetbrains.php.lang.psi.resolve.types.PhpTypeProvider3;
 import cz.juzna.intellij.nette.utils.ComponentSearcher;
 import cz.juzna.intellij.nette.utils.ElementValueResolver;
 import cz.juzna.intellij.nette.utils.PhpIndexUtil;
-import cz.juzna.intellij.nette.utils.PhpPsiUtil;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Set;
 
 
-public class ComponentTypeProvider implements PhpTypeProvider2 {
+public class ComponentTypeProvider implements PhpTypeProvider3
+{
 
-	final static String SEPARATOR = "\u0180";
-	final static String TYPE_SEPARATOR = "\u0181";
+	private final static String SEPARATOR = "\u0180";
 
 	@Override
 	public char getKey() {
@@ -34,16 +34,13 @@ public class ComponentTypeProvider implements PhpTypeProvider2 {
 
 	@Nullable
 	@Override
-	public String getType(PsiElement el) {
+	public PhpType getType(PsiElement el) {
 		if (DumbService.getInstance(el.getProject()).isDumb()) {
 			return null;
 		}
 		String componentName;
 		PhpType type;
 		if (el instanceof ArrayAccessExpression) {
-			if (PhpPsiUtil.isLocallyResolvableType(el)) {
-				return null;
-			}
 			ArrayIndex index = ((ArrayAccessExpression) el).getIndex();
 			if (index == null || !(el.getFirstChild() instanceof PhpTypedElement)) {
 				return null;
@@ -66,21 +63,23 @@ public class ComponentTypeProvider implements PhpTypeProvider2 {
 		if (componentName == null) {
 			return null;
 		}
-
-		return componentName + SEPARATOR + type.toString().replaceAll("\\|", TYPE_SEPARATOR);
+		PhpType resultType = new PhpType();
+		for (String typePart : type.getTypes()) {
+			resultType.add("#" + getKey() + componentName + SEPARATOR + typePart);
+		}
+		return resultType;
 	}
 
 	@Override
-	public Collection<? extends PhpNamedElement> getBySignature(String s, Project project) {
-
-		String[] parts = s.split(SEPARATOR, 2);
+	public Collection<? extends PhpNamedElement> getBySignature(String expression, Set<String> visited, int depth, Project project)
+	{
+		String[] parts = expression.split(SEPARATOR, 2);
 		if (parts.length != 2) {
 			return Collections.emptyList();
 		}
 		String componentName = parts[0];
-		Collection<PhpClass> classes = PhpIndexUtil.getByType(parts[1].split(TYPE_SEPARATOR), PhpIndex.getInstance(project));
+		Collection<PhpClass> classes = PhpIndexUtil.getByType((new PhpType()).add(parts[1]), PhpIndex.getInstance(project));
 		return ComponentSearcher.findMethods(new ComponentSearcher.ComponentQuery(componentName, classes));
-
 	}
 
 }
